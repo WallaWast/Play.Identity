5e0f0fbaf0ce854e5bf6dcfa784916680a47b837
Wallace Torres <wallacewst@gmail.com>
2023-11-26 16:30:23 -0800

    Added seq support

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -15,6 +15,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using Play.Common.HealthChecks;
+using Play.Common.Logging;
 using Play.Common.MassTransit;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
@@ -80,6 +81,8 @@ namespace Play.Identity.Service
                 options.KnownNetworks.Clear();
                 options.KnownProxies.Clear();
             });
+
+            services.AddSeqLogging(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
